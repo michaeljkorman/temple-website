616dd6ef2959df06304526db2aec122a9649afe6
Michael Korman <michael@michaelkorman.com>
2025-12-09 10:34:36 -0500

    Cleaned up some comments, changed under-icons-text color, and added GB to GDPR countries.

--- js/klaro-config.js
+++ js/klaro-config.js
@@ -138,10 +138,11 @@ fetch('https://ipapi.co/json')
         const country = data.country_code;
         const region = data.region_code;
 
+        // EU countries + United Kingdom
         const euCountries = [
             'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE',
             'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT',
-            'RO', 'SK', 'SI', 'ES', 'SE'
+            'RO', 'SK', 'SI', 'ES', 'SE', 'GB'
         ];
 
         if (euCountries.includes(country)) {
